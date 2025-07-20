25d97e99e13861bfdb75eceed925b90092bb777c
Hyonie <dev.hyonie@gmail.com>
2025-07-20 22:18:45 +0900

    Refactor: 빌드 파일

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './SCSS/global.scss'
+import './scss/global.scss'
 import { ModalContext } from './contexts/ModalContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
