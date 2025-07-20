cf9331c162d01e8cfa23cb89654ed71a33f6e0de
Hyonie <dev.hyonie@gmail.com>
2025-07-20 23:22:55 +0900

    Feat: 배포 서버

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
     // 1. 웨딩 데이터 호출
     useEffect(()=>{
       setLoading(true);
-      fetch('http://localhost:8888/wedding').then((res)=>{
+      fetch('https://with-chu-forever-1.onrender.com/wedding').then((res)=>{
         if(res.ok === false){
           throw new Error('청첩장 정보를 불러오지 못했습니다.');
         }
